9afe3b7121d56c6be81acf694acf83b3e5b2f0c3
Antonio Hernandez <112008621+HernandezA1007@users.noreply.github.com>
2023-02-12 18:27:38 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -9,10 +9,11 @@
 require('database.php');
 
 
-$query = 'SELECT customers.customerID, customers.emailAddress, customers.firstName, 
-            customers.lastName, addresses.line1, addresses.city, addresses.state, 
-            addresses.zipCode, addresses.phone from customers INNER JOIN addresses 
-            on customers.customerID = addresses.customerID';// PUT YOUR SQL QUERY HERE
+$query = 'SELECT customers.customerID, customers.emailAddress, customers.firstName, customers.lastName, 
+            addresses.line1, addresses.city, addresses.state, addresses.zipCode, addresses.phone 
+            FROM customers 
+            INNER JOIN addresses 
+            ON customers.customerID = addresses.customerID';// PUT YOUR SQL QUERY HERE
 // Example: $query = 'SELECT * FROM customers';
 
 $statement = $db->prepare($query);
